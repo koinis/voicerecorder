ac1db7c0b90140e02995cc9b14ca4423e9d4d849
mrpol <pkoinis8@gmail.com>
2021-04-27 02:08:41 +0300

    Add files via upload

---
+++ recorder.py
@@ -0,0 +1,18 @@
+import sounddevice as sound
+from scipy.io.wavfile import write
+import wavio as wav
+import sys
+
+try:
+    freq = 44100            
+    duration = 1800              #The duration of recording in seconds (It can be changed)
+    #print("recording...")
+    recording = sound.rec(int(duration*freq), samplerate=freq, channels=2)
+
+    sound.wait()
+    write("recording.wav",freq,recording)       #Creates the .wav file
+
+except KeyboardInterrupt:          #Passes keyboard interrupts
+    pass
+
+
